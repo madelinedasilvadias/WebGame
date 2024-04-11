53f8ec2adffd94df03b0dfc15b3edc9d60afff2e
madelinedasilvadias <114189247+madelinedasilvadias@users.noreply.github.com>
2024-04-11 15:09:37 +0200

    delete Game

--- Game/GalacticJumper.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var canvas = document.getElementById("gameCanvas");
-    var context = canvas.getContext("2d");
-
-    context.fillStyle = "white";
-    context.font = "30px Arial";
-    context.textAlign = "center";
-    context.fillText("Coming soon", canvas.width / 2, canvas.height / 2);
-});
